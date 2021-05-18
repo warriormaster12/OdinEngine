bd437ddd9c6fc1d2b118d610db6de3a5ffffd0de
warriormaster12 <streng.alexander@gmail.com>
2021-05-18 20:41:15 +0300

    feature: adding multiple meshes

--- Engine/Source/Editor/meshAdder.cpp
+++ Engine/Source/Editor/meshAdder.cpp
@@ -2,34 +2,42 @@
 
 #include "glm/gtx/transform.hpp"
 #include "imgui.h"
+#include "logger.h"
 #include "renderobject.h"
 
 #include <glm/glm.hpp>
 
 #include <string>
 
-Mesh mesh3;
 
-RenderObject barrelObj3;
+
 
 char textBuffer[256];
+glm::vec3 translation;
 void MeshAdder::ShowMeshAdderWindow()
 {
     static bool isActive = false;
     static std::string filePath;
     ImGui::Begin("Mesh Adder", &isActive, ImGuiWindowFlags_NoCollapse);
     ImGui::InputText("File Path", textBuffer, IM_ARRAYSIZE(textBuffer));
+    
+    ImGui::Text("Translation");
+    ImGui::InputFloat("x", &translation.x);
+    ImGui::InputFloat("y", &translation.y);
+    ImGui::InputFloat("z", &translation.z);
     filePath = textBuffer;
     if(ImGui::Button("Apply"))
     {
-        mesh3.LoadFromObj(filePath);
-        mesh3.CreateMesh();
+        RenderObject rObj;
+    
 
-        barrelObj3.p_mesh = &mesh3;
-        barrelObj3.material = "main mat";
-        barrelObj3.transformMatrix = glm::translate(glm::vec3(1.0f, 1.0f, 1.0f));
+        rObj.p_mesh = new Mesh;
+        rObj.p_mesh->LoadFromObj(filePath);
+        rObj.p_mesh->CreateMesh();
+        rObj.material = "main mat";
+        rObj.transformMatrix = glm::translate(translation);
 
-        ObjectManager::PushObjectToQueue(barrelObj3);
+        ObjectManager::PushObjectToQueue(rObj);
     }
     //ImGui::InputText("file path", textBuffer, 60);
     ImGui::End();
